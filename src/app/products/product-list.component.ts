70303d0fec18ce5af833ae2d2a17b4d1c58cf798
Milos Budalic <milos.budalic@persida-bio.com>
2020-12-14 22:41:20 +0100

    Init commit...

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,65 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { BehaviorSubject, combineLatest, EMPTY, Subject } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { ProductCategoryService } from '../product-categories/product-category.service';
+import { ProductService } from './product.service';
+
+@Component({
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductListComponent {
+  pageTitle = 'Product List';
+  private errorMessageSubject = new Subject<string>();
+  errorMessage$ = this.errorMessageSubject.asObservable();
+
+  private categorySelectedSubject = new BehaviorSubject<number>(0);
+  categorySelectedAction$ = this.categorySelectedSubject.asObservable();
+
+  products$ = combineLatest([
+    this.productService.productsWithAdd$,
+    this.categorySelectedAction$
+  ])
+    .pipe(
+      map(([products, selectedCategoryId]) =>
+        products.filter(product =>
+            selectedCategoryId ? product.categoryId === selectedCategoryId : true
+          )
+      ),
+      catchError(err => {
+        this.errorMessageSubject.next(err);
+        return EMPTY;
+      })
+    );
+
+  categories$ = this.productCategoryService.productCategories$
+      .pipe(
+        catchError(err => {
+          this.errorMessageSubject.next(err);
+          return EMPTY;
+        })
+      );
+
+      // Simple filter
+    // productsSimpleFilter$ = this.productService.productsWithCategory$
+    //   .pipe(
+    //     map(products =>
+    //       products.filter(product =>
+    //         this.selectedCategoryId ? product.categoryId === this.selectedCategoryId : true
+    //         )
+    //       )
+    //   );
+
+  constructor(private productService: ProductService,
+              private productCategoryService: ProductCategoryService) { }
+
+  onAdd(): void {
+    this.productService.addProduct();
+  }
+
+  onSelected(categoryId: string): void {
+    // this.selectedCategoryId = +categoryId;
+    this.categorySelectedSubject.next(+categoryId);
+  }
+}
